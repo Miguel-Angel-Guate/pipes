31df175e8b00736e05f310dd217fb7bc555eea60
Miguel Angel <miguel.teletor@atmira.com>
2020-10-27 16:18:51 +0100

    fix: added pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   name: string = 'Louise Glück ';
   smallArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI: number = Math.PI;
+  percentage: number = 0.235;
+  monthlyPayment: number = 1234.6;
 }
